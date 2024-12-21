7c1fff2f0acbcd2511f739bb1bf9fc2e2c2ae76e
Zavis <thezavis@gmail.com>
2024-12-21 22:28:42 +1100

    Node install Cookies and Axios, authService implement Axios interceptors and save tokens to Cookies

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,23 +1,46 @@
+import axios from "axios";
+import Cookies from 'js-cookie';
+
 const API_URL = 'http://localhost:8080/api/v1'
 
-export const login = async (data) => {
-    try{
-        const res = await fetch(`${API_URL}/login`, {
-            method: 'POST', 
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify(data)
-        })
-        if(!res.ok){
-            const error = await res.json();
-            throw new Error(error.error || "Login Failed")
+axios.interceptors.request.use(
+    (config) => {
+        const accessToken = Cookies.get('accessToken');
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`
         }
-        const result = await res.json();
-        return result;
-    }catch (error){
-        return {error: true, message : error.message}
-    }
-   
+        return config
 
+    },
+    (error) => { return Promise.reject(error) }
+)
+
+export const login = async (data) => {
+    try {
+        //axios makes data from object -> JSON
+        const res = await axios.post(`${API_URL}/login`, data, {
+            // method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            // body: JSON.stringify(data)
+        })
+        
+        const result = await res.data;
+        console.log(result.accessToken);
+        if (result.accessToken) {
+            Cookies.set('accessToken', result.accessToken, { expires: 1 / 24})
+            Cookies.set('refreshToken', result.refreshToken, { expires: 7})
+        }
+        return result;
+    } catch (error) {
+        if (error.response) {
+            //server error
+            const errorMessage = error.response.data.error || "Login Failed"
+            return { error: true, message: errorMessage }
+        }else{
+            //client error
+            return { error: true, message: error.message };
+        }
+    }
 
 }
 
@@ -49,13 +72,13 @@ export const register = async (data) => {
 export const verifyEmail = async (email, code) => {
     try {
 
-        const res = await fetch (`${API_URL}/verify-email?email=${email}&code=${code}`, {
-            method: 'POST', 
-            headers: {'Content-Type' : 'application/json'},
+        const res = await fetch(`${API_URL}/verify-email?email=${email}&code=${code}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({})
         });
-        
-        if(!res.ok){
+
+        if (!res.ok) {
             const error = await res.json();
             throw new Error(error.error || "Validation failed")
         }
@@ -70,12 +93,12 @@ export const verifyEmail = async (email, code) => {
 export const resendValidationCode = async (email) => {
     try {
 
-        const res = await fetch (`${API_URL}/resend-verification-code?email=${email}`, {
-            method: 'POST', 
-            headers: {'Content-Type' : 'application/json'},            
+        const res = await fetch(`${API_URL}/resend-verification-code?email=${email}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
         });
-        
-        if(!res.ok){
+
+        if (!res.ok) {
             const error = await res.json();
             throw new Error(error.error || "Validation failed")
         }
