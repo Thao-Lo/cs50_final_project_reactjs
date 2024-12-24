0b9819076deec16b144cd18439caec372db52adf
Zavis <thezavis@gmail.com>
2024-12-25 01:37:53 +1100

    BookingPage SlotCom generate slots, and rearrange it by date

---
+++ src/component/SlotComponent/index.js
@@ -0,0 +1,25 @@
+import { Box, Card, CardActionArea, CardContent, Typography } from "@mui/material";
+
+
+
+function SlotComponent({ slot }) {
+
+    return (
+        <Box sx={{mb: 1}}>
+            <Card sx={{ width: '8rem'}}>
+                <CardActionArea>
+                    <CardContent sx={{padding:'0.5rem' }}>
+                        <Typography gutterBottom variant="body" component="div" sx={{textAlign: 'center'}}>
+                            {slot.time}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary', overflow:'hidden',  textOverflow: "ellipsis" }}>
+                            {slot.tableName} - Dinning
+                        </Typography>
+                       
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Box>
+    );
+}
+export default SlotComponent;
--- src/pages/BookingPage/index.js
+++ src/pages/BookingPage/index.js
@@ -4,7 +4,9 @@ import BookingComponent from "../../component/BookingComponent";
 import { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import { getSlots } from "../../services/bookingService";
+import SlotComponent from "../../component/SlotComponent";
 
+//https://day.js.org/en/
 dayjs.extend(utc);
 dayjs.extend(timezone)
 
@@ -25,18 +27,18 @@ function BookingPage() {
         if (result.error) {
             console.log(result.message);
             setSlots([])
-            setError(result.message)           
+            setError(result.message)
             return;
         }
         setSlots(result.availableSlots)
-        setError(''); 
+        setError('');
     }
 
     useEffect(() => {
         fetchSlot();
     }, [bookingValues.capacity, bookingValues.date, bookingValues.time])
 
-   
+    console.log(slots);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -51,15 +53,64 @@ function BookingPage() {
             date: newDate
         }))
     }
-    const slotList = (slots.length > 0) ? (slots.map((slot) => (<Box>{slot.id}</Box>))) : (error ? <Box>{error}</Box> : <Box>No result</Box>)
+
+    const groupSlotsByDate = (slots) => {
+        return slots.reduce((acc, slot) => {
+            if (!acc[slot.date]) {
+                acc[slot.date] = []
+            }
+            acc[slot.date].push(slot)
+            return acc;
+        }, {})
+    }
+    const groupSlots = groupSlotsByDate(slots)
+
+
+    const slotList = (slots.length > 0) ?
+        (Object.keys(groupSlots).map((date) => (
+            <Box key={date}>
+                <Box sx={{}}>{date}</Box>
+                <Box sx={{
+                    display: 'flex', gap: '0.5rem',
+                    overflow: 'auto',
+                    whiteSpace: "nowrap",
+                    "&::-webkit-scrollbar": {
+                        width: '1rem',
+                        height: '0.5rem'
+                    },
+                    "&::-webkit-scrollbar-track": {
+                        backgroundColor: "#f1f1f1", // Background of the scrollbar track
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                        backgroundColor: "#888", // Color of the scrollbar thumb
+                        borderRadius: "10px",    // Rounded corners
+                    },
+                    "&::-webkit-scrollbar-thumb:hover": {
+                        backgroundColor: "#555", // Color on hover
+                    }
+                }}>
+                    {
+                        groupSlots[date].map((slot) => (
+                            <SlotComponent key={slot.id} slot={slot} />
+                        ))
+                    }
+                </Box>
+
+            </Box>
+        ))) : (error ? <Box>{error}</Box> : <Box>No result</Box>)
+
+
 
     return (
         <Box>
             <BookingComponent bookingValues={bookingValues} handleDateChange={handleDateChange} handleInputChange={handleInputChange}
 
             />
-            {slotList}
-        </Box>
+            <Box>
+                {slotList}
+            </Box>
+
+        </Box >
     )
 
 }
