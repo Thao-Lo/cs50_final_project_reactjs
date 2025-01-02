321984c5f89e1edb70738f2c184d1177554a38e8
Zavis <thezavis@gmail.com>
2025-01-03 00:07:28 +1100

    NavBar add filteredPages to Navbar for rendering route based on authentication and user role, cannot import css file StripeStyles to CheckoutForm

--- src/component/NavBarReservation/index.js
+++ src/component/NavBarReservation/index.js
@@ -1,20 +1,18 @@
-import { useState } from 'react';
+import AdbIcon from '@mui/icons-material/Adb';
+import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import { NavLink, useNavigate } from 'react-router-dom';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { USER_ACTION, useUser } from '../../hooks/UserContext';
 import { logout } from '../../services/authService';
-import { useUser } from '../../hooks/UserContext';
 
 
 const pages = [{
@@ -48,13 +46,54 @@ function NavBarReservation() {
         setAnchorElNav(event.currentTarget);
     };
     const handleCloseNavMenu = () => {
-
         setAnchorElNav(null);
     };
+    //remove Cookies and reset to initialState in UserContext
     const handleMenuClick = (label) => {
         if (label === 'Logout') {
+            dispatch({ type: USER_ACTION.LOGOUT })
             logout();
         }
+    }
+    const getFilteredPages = () => {
+        //if user not login
+        if (!isAuthenticated) {
+            return pages.filter(({ label }) => !['My Account', 'Dashboard', 'Logout'].includes(label))
+        }
+        //if user login and have role "ADMIN"
+        if (user?.role === 'ADMIN') {
+            return pages.filter(({ label }) => label !== 'Login' && label !== 'My Account')
+        }
+        //role "GUEST"
+        return pages.filter(({ label }) => label !== 'Login' && label !== 'Dashboard')
+    }
+    const filteredPages = getFilteredPages();
+
+    // for Mobile view
+    const renderMenuItems = () => {
+        return filteredPages.map(({ label, route }) => (
+            <MenuItem key={label} onClick={() => { handleCloseNavMenu(); handleMenuClick(label) }}>
+                <Typography
+                    component={NavLink} to={route}
+                    sx={{ textAlign: 'center', textDecoration: 'none', color: 'inherit' }}>
+                    {label}
+                </Typography >
+            </MenuItem>
+        ))
+    }
+    //for desktop view 
+    const renderButtonItems = () => {
+        return filteredPages.map(({ label, route }) => (
+            <Button
+                component={NavLink}
+                to={route}
+                key={label}
+                onClick={() => { handleCloseNavMenu(); handleMenuClick(label) }}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+                {label}
+            </Button>
+        ))
     }
     return (
         <AppBar position="static">
@@ -106,17 +145,8 @@ function NavBarReservation() {
                             onClose={handleCloseNavMenu}
                             sx={{ display: { xs: 'block', md: 'none' } }}
                         >
-                            
-
-                            {pages.map(({ route, label }) => (
-                                <MenuItem key={label} onClick={() => { handleCloseNavMenu(); handleMenuClick(label) }}>
-                                    <Typography
-                                        component={NavLink} to={route}
-                                        sx={{ textAlign: 'center', textDecoration: 'none', color: 'inherit' }}>
-                                        {label}
-                                    </Typography >
-                                </MenuItem>
-                            ))}
+                            {/* MOBILE VIEW */}
+                            {renderMenuItems()}
                         </Menu>
                     </Box>
                     <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
@@ -138,18 +168,10 @@ function NavBarReservation() {
                     >
                         LOGO
                     </Typography>
+                    {/* DESKTOP VIEW */}
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map(({ route, label }) => (
-                            <Button
-                                component={NavLink}
-                                to={route}
-                                key={label}
-                                onClick={() => { handleCloseNavMenu(); handleMenuClick(label) }}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {label}
-                            </Button>
-                        ))}
+                        {renderButtonItems()}
+
                     </Box>
                 </Toolbar>
             </Container>
