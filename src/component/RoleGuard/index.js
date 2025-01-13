70395e8d8f16a1ac1a77c59c5de26785dcef501b
Zavis <thezavis@gmail.com>
2025-01-13 18:41:58 +1100

    RoleGuard implement RoleGuard to navidate to route based on userContext

---
+++ src/component/RoleGuard/index.js
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom";
+import { useUser } from "../../hooks/UserContext";
+
+
+function RoleGuard({ allowedRole }) {
+    const { state: { user, isAuthenticated } } = useUser();
+
+    if (!isAuthenticated) {
+        return <Navigate to="/" replace />
+    }
+    if (allowedRole.includes(user.role)) {
+        return <Outlet />
+    }
+    return <Navigate to="/403" replace />
+
+}
+export default RoleGuard;
