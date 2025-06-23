0159a0b1f9c3b15b257745dbc2614f2fe0d9b7b6
zavis <thezavis@gmail.com>
2025-06-23 23:05:20 +1000

    AWS: sample S3 service for uploading file

---
+++ src/services/s3Service.js
@@ -0,0 +1,12 @@
+// import axiosInstance from "./axiosInstance";
+
+// export const uploadFileToS3 = (file) => {
+//     const formData = new FormData();
+//     formData.append("file", file);
+
+//     return axiosInstance.post("/s3/upload", formData, {
+//         headers: {
+//             "Content-Type" : "multipart/form-data",
+//         },
+//     });
+// };
